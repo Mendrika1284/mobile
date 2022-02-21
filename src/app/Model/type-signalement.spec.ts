3ef5eef8613fe8b3e5be50ffdcebb1d5c18c2b5c
Mendrika1284 <mendrikaitokiuana@gmail.com>
2022-02-21 15:33:35 +0300

    .

---
+++ src/app/Model/type-signalement.spec.ts
@@ -0,0 +1,7 @@
+import { TypeSignalement } from './type-signalement';
+
+describe('TypeSignalement', () => {
+  it('should create an instance', () => {
+    expect(new TypeSignalement()).toBeTruthy();
+  });
+});
--- src/app/typesignalement.service.ts
+++ src/app/typesignalement.service.ts
@@ -1,9 +1,21 @@
+/* eslint-disable @typescript-eslint/member-ordering */
+/* eslint-disable @typescript-eslint/naming-convention */
 import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders} from '@angular/common/http';
+import { HttpParams} from '@angular/common/http';
+import { TypeSignalement } from './Model/type-signalement';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TypesignalementService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  Url2='http://localhost:8081/api/typeSignalement';
+
+  getAllTypeSignalement()
+  {
+    return this.http.get<TypeSignalement[]>(this.Url2);
+  }
 }
